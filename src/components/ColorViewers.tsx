58c844b7ad7e46bcd81700d8a41351612ce970d7
Anthony Alicea <alicea.9a@gmail.com>
2025-10-20 19:25:39 -0600

    Refactor: Move elements into new component

---
+++ src/components/ColorViewers.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ColorViewers = () => {
+  return (
+    <div className="colorBlobs">
+            <div className="colorSwatch">color selected</div>
+            <div className="colorSwatch">color hovering</div>
+          </div>
+  )
+}
+
+export default ColorViewers
